3a2cd5ca98e0d8ad5a24f21979d8f6aab633f82c
Wyatt Greenway <th317erd@gmail.com>
2017-01-24 13:17:55 -0700

    Moved appUtils inside utils

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const utils = require('./lib/utils.js');
-
 module.exports = {
-	utils: utils
+	utils: require('./lib/utils.js'),
+	data: require('./lib/data.js'),
+	api: require('./lib/utils.js'),
 };
