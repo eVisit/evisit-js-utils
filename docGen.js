9b6cf9e9fd28556c50e11e887b88ee7c1c04f0b0
Wyatt Greenway <th317erd@gmail.com>
2017-02-03 15:47:35 -0700

    Added utils.equal function for deep compairison

---
+++ docGen.js
@@ -0,0 +1,9 @@
+module.exports = function() {
+  return {
+    include: ['^lib/.*'],
+    exclude: [],
+    extensions: ['js'],
+    inputPath: './',
+    outputPath: '../eVisit-Core-JS.wiki'
+  };
+};
